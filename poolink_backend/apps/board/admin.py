7fd96a9a95ba5733a25ee0f9e18ae8fd73aeb0c8
jaethewiederholen <herakjy0705@gmail.com>
2021-06-24 22:27:33 +0900

    blank board/admin

--- poolink_backend/apps/board/admin.py
+++ poolink_backend/apps/board/admin.py
@@ -1,8 +1,3 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from poolink_backend.apps.board.models import Board
-
-
-@admin.register(Board)
-class BoardAdmin(admin):
-    list_display = ("name", "email")
+# from poolink_backend.apps.board.models import Board
