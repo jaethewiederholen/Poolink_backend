a27d9e44a1b1b5b379b8ab149039bf0647d5e71e
julia-ing <ywchoi0625@gmail.com>
2021-07-01 02:42:48 +0900

    UserManager 함수들 수정 - 어드민 로그인 성공, 실명 입력칸 안뜸

--- poolink_backend/apps/users/models.py
+++ poolink_backend/apps/users/models.py
@@ -10,15 +10,44 @@ from poolink_backend.bases.models import Model
 
 
 class UserManager(BaseUserManager):
-    def create_superuser(self, email, password, **extra_fields):
-        return super().create_superuser(email=email, password=password, **extra_fields)
+    # def create_superuser(self, email, password, **extra_fields):
+    #     return super().create_superuser(email=email, password=password, **extra_fields)
+    #
+    # def create_user(self, username=None, email=None, password=None, **extra_fields):
+    #     return super().create_superuser(
+    #         username=username, email=email, password=password, **extra_fields
+    #     )
+
+    use_in_migrations = True
 
     def create_user(self, username=None, email=None, password=None, **extra_fields):
-        return super().create_superuser(
-            username=username, email=email, password=password, **extra_fields
+        if not email:
+            raise ValueError('must have user email')
+        if not password:
+            raise ValueError('must have user password')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            password=password
         )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    def get_or_create_user(self, username=None, name=None, email=None, password=None):
+    def create_superuser(self, email, username, password, **extra_fields):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+def get_or_create_user(self, username=None, name=None, email=None, password=None):
         users = self.model.objects.filter(email=email)
         is_created = False
         if users.exists():
