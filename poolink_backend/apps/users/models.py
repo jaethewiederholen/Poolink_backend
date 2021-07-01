4735a74686d7afaa0ddddb0a4020c6c6e93f2b74
jaethewiederholen <herakjy0705@gmail.com>
2021-07-01 23:46:25 +0900

    user model error fixed

--- poolink_backend/apps/users/models.py
+++ poolink_backend/apps/users/models.py
@@ -10,13 +10,33 @@ from poolink_backend.bases.models import Model
 
 
 class UserManager(BaseUserManager):
-    def create_superuser(self, email, password, **extra_fields):
-        return super().create_superuser(email=email, password=password, **extra_fields)
+    use_in_migrations = True
 
-    def create_user(self, username=None, email=None, password=None, **extra_fields):
-        return super().create_superuser(
-            username=username, email=email, password=password, **extra_fields
+    def create_user(
+        self, username=None, email=None, password=None, name=None, **extra_fields
+    ):
+        if not email:
+            raise ValueError("must have user email")
+        if not password:
+            raise ValueError("must have user password")
+        user = self.model(
+            email=self.normalize_email(email), username=username, name=name
         )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, name, **extra_fields):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            name=name,
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
 
     def get_or_create_user(self, username=None, name=None, email=None, password=None):
         users = self.model.objects.filter(email=email)
@@ -64,7 +84,7 @@ class User(AbstractBaseUser, Model, PermissionsMixin):
 
     objects = UserManager()
     USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
+    REQUIRED_FIELDS = ["email", "name"]
 
     class Meta:
         verbose_name = verbose_name_plural = "유저"
