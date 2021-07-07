223b29a561f0ea056e3b9af71f3f9f025888b775
julia-ing <ywchoi0625@gmail.com>
2021-07-08 02:02:37 +0900

    category 전체 리스트 api

--- poolink_backend/apps/category/api/serializers.py
+++ poolink_backend/apps/category/api/serializers.py
@@ -1,12 +1,11 @@
 from poolink_backend.apps.category.models import Category
+from poolink_backend.apps.board.api.serializers import (
+    BoardSerializer
+)
 from poolink_backend.bases.api.serializers import ModelSerializer
 
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = (
-            "name",
-            "image",
-            "color",
-        )
+        fields = ['name', 'image', 'color']
--- poolink_backend/apps/category/api/views.py
+++ poolink_backend/apps/category/api/views.py
@@ -1,3 +1,10 @@
+from django.utils.translation import ugettext_lazy as _
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import APIView as BaseAPIView
+
 from poolink_backend.apps.category.api.serializers import CategorySerializer
 from poolink_backend.apps.category.models import Category
 from poolink_backend.bases.api.viewsets import ModelViewSet
@@ -5,8 +12,15 @@ from poolink_backend.bases.api.viewsets import ModelViewSet
 from rest_framework import decorators
 
 
-class CategoryViewSet(ModelViewSet):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-    filterset_fields = ["name", "color"]
+class CategoryList(BaseAPIView):
+    @swagger_auto_schema(
+        operation_id=_("Get Category List"),
+        operation_description=_("온보딩 화면에 보일 카테고리 전체 리스트입니다."),
+        responses={200: openapi.Response(_("OK"), CategorySerializer, )},
+        tags=[_("카테고리"), ],
+    )
+    def get(self, request, format=None):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
 
