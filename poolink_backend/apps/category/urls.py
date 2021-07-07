31a4e929b1a76a867d00ecceb0b30751a0a80027
julia-ing <ywchoi0625@gmail.com>
2021-07-08 02:02:37 +0900

    category 전체 리스트 api

---
+++ poolink_backend/apps/category/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from poolink_backend.apps.category.api import views
+
+app_name = "category"
+urlpatterns = [
+    path("", views.CategoryList.as_view(), name='categories'),
+]
