f5d4caa9d7ae215845e47ffba2e267d6ac8eafbf
julia-ing <ywchoi0625@gmail.com>
2021-07-09 06:47:10 +0900

    category/select api - without serializer

--- poolink_backend/apps/category/urls.py
+++ poolink_backend/apps/category/urls.py
@@ -4,4 +4,5 @@ from poolink_backend.apps.category.api import views
 app_name = "category"
 urlpatterns = [
     path("", views.CategoryList.as_view(), name='categories'),
+    path("select/", views.CategorySelectView.as_view(), name='category-select'),
 ]
