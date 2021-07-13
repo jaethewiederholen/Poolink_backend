1649635d37bf6b9a871dff3afcf2a6c4a62d77f9
julia-ing <ywchoi0625@gmail.com>
2021-07-13 23:05:19 +0900

    modify uris to plural

--- poolink_backend/apps/category/urls.py
+++ poolink_backend/apps/category/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from poolink_backend.apps.category.api import views
 
-app_name = "category"
+app_name = "categories"
 urlpatterns = [
     path("", views.CategoryList.as_view(), name='categories'),
     path("/choice", views.CategorySelectView.as_view(), name='category-select'),
