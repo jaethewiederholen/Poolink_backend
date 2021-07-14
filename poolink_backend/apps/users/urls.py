a9ccf3365995bceddebd0804affee25fe9f67ba8
julia-ing <ywchoi0625@gmail.com>
2021-07-15 04:36:11 +0900

    remove id from user logout, delete apis

--- poolink_backend/apps/users/urls.py
+++ poolink_backend/apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .api.views import google_login_view
+from .api.views import google_login_view, user_delete_view, user_logout_view
 
 app_name = "users"
 urlpatterns = [
+    path("", view=user_delete_view),
+    path("/logout", view=user_logout_view),
     path("/googlelogin", google_login_view),
 ]
