965a6b3594d520ed2dc064e8507c30bcf41d77fd
jaethewiederholen <herakjy0705@gmail.com>
2021-08-28 20:05:19 +0900

    allaowed host

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 # ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "192.168.219.122", "*",
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*",
                  "Poolink-dev-env.ap-northeast-2.elasticbeanstalk.com"]
 # DATABASES
 # ------------------------------------------------------------------------------
