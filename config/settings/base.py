ca9002beaa893e4d55da56ee67342c2519e6ffda
jaethewiederholen <herakjy0705@gmail.com>
2021-09-15 23:23:04 +0900

    revert cors setting for dev server

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,7 +8,6 @@ from pathlib import Path
 from typing import List
 
 import environ
-from corsheaders.defaults import default_methods
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # poolink_backend/
@@ -348,15 +347,13 @@ REST_FRAMEWORK = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:3000',
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:3000',
 
-#                         'http://localhost:3000',
-#                         'https://127.0.0.1:3000',
-#                         'https://localhost:3000',
-#                         ]
-
-CORS_ALLOW_METHDOS = default_methods
+                        'http://localhost:3000',
+                        'https://127.0.0.1:3000',
+                        'https://localhost:3000',
+                        ]
 CORS_ALLOW_CREDENTIALS = True
 
 # Google Login
