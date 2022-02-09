d0b78ad1070b2352ca61f284a9ebeea662861ae5
jaethewiederholen <herakjy0705@gmail.com>
2022-02-09 05:09:49 +0000

    additional serializer for private link disclosure

--- poolink_backend/apps/link/api/serializers.py
+++ poolink_backend/apps/link/api/serializers.py
@@ -4,19 +4,38 @@ from poolink_backend.apps.link.models import Link
 from poolink_backend.bases.api.serializers import ModelSerializer
 
 
-class FilteredLinkSerializer(serializers.ListSerializer):
+class CurtLinkSerializer(serializers.ListSerializer):
 
     def to_representation(self, data):
-        data = data.order_by('-id')[:2]
-        return super(FilteredLinkSerializer, self).to_representation(data)
+        data = data.filter(show=True).order_by('-id')[:2]
+        return super(CurtLinkSerializer, self).to_representation(data)
 
 
 class LinkInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
-        list_serializer_class = FilteredLinkSerializer
+        list_serializer_class = CurtLinkSerializer
         model = Link
         fields = ['board', 'label', 'url', 'favicon', 'meta_image']
+
+
+class FilteredLinkSerializer(serializers.ListSerializer):
+
+    def to_representation(self, data):
+        data = data.filter(show=True)
+        return super(FilteredLinkSerializer, self).to_representation(data)
+
+
+class PublicLinkSerializer(ModelSerializer):
+    link_id = serializers.SerializerMethodField()
+
+    class Meta:
+        list_serializer_class = FilteredLinkSerializer
+        model = Link
+        fields = ['link_id', 'board', 'label', 'url', 'show', 'favicon', 'meta_image', 'memo']
+
+    def get_link_id(self, instance):
+        return instance.id
 
 
 class LinkSerializer(ModelSerializer):
